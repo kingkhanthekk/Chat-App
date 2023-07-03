10312906122642c9f2f4e9f15633507d0f27c192
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2023-07-03 14:55:17 +0300

    Initialize app

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server running on port " + port);
+});
