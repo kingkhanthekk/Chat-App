33dfa87077db29eace9f74117ffe420209dccda9
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2023-07-03 15:22:07 +0300

    Initialize socket.io

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const http = require("http");
 const express = require("express");
-const app = express();
 const path = require("path");
+const socketio = require("socket.io");
+
+const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+io.on("connection", () => {
+  console.log("WebSocket Server");
 });
 
 const port = process.env.PORT || 3000;
